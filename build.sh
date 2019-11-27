e7283b9661d5a00e2e2faf2432e78ec7008cec49
Assis Ngolo <figassis@gmail.com>
2019-11-27 16:09:12 +0100

    debugging

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ if [ ! -z "$1" ]; then
 fi;
 
 rm -rf $PKG/bin && mkdir -p $PKG/bin/linux_amd64
-GOOS=linux go build -ldflags "-X ${PKG}/pkg/version.buildVersion=${tag}" -o $PKG/bin/linux_amd64/mysql-agent $PKG/cmd/mysql-agent
-GOOS=linux go build -ldflags "-X ${PKG}/pkg/version.buildVersion=${tag}" -o $PKG/bin/linux_amd64/mysql-operator $PKG/cmd/mysql-operator
+GOOS=linux go build -ldflags "-X github.com/oracle/mysql-operator/pkg/version.buildVersion=${tag}" -o $PKG/bin/linux_amd64/mysql-agent $PKG/cmd/mysql-agent
+GOOS=linux go build -ldflags "-X github.com/oracle/mysql-operator/pkg/version.buildVersion=${tag}" -o $PKG/bin/linux_amd64/mysql-operator $PKG/cmd/mysql-operator
 docker build --rm --build-arg=http_proxy --build-arg=https_proxy -t figassis/mysql-agent:$tag -f $PKG/docker/mysql-agent/Dockerfile . && docker push figassis/mysql-agent:$tag
 docker build --rm --build-arg=http_proxy --build-arg=https_proxy -t figassis/mysql-operator:$tag -f $PKG/docker/mysql-operator/Dockerfile .  && docker push figassis/mysql-operator:$tag
